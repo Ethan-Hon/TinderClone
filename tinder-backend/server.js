51d34edd8cb114bc27d306393eb673a00da37e0c
Hanna0401 <all-match@outlook.com>
2022-01-20 00:41:42 +0800

    modified some backend issue

---
+++ tinder-backend/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+//App Config
+const server = require('express');
+
+//MiddleWares
+
+//DB Configuration
+
+//API Endpoints
+
+//Listener
