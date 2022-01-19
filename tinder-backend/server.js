0a6d8345fffb3b24edbfe0b0814b1d47b405d27f
Hanna0401 <all-match@outlook.com>
2022-01-20 00:58:12 +0800

    added backend initial data

--- tinder-backend/server.js
+++ tinder-backend/server.js
@@ -2,12 +2,15 @@ import express from 'express';
 import mongoose from 'mongoose';
 
 //App Config
-const server = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
 
 //MiddleWares
 
 //DB Configuration
 
 //API Endpoints
+app.get("/", (req, res) => res.status(200).send('Hey Guy'))
 
 //Listener
+app.listen(port, () => console.log(`listening on localhost: ${port}`))
